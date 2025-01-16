b7b4367b6c84858c89b451247ced4b21fbb7845b
Cordell Bloor <Cordell.Bloor@amd.com>
2025-01-16 14:27:14 -0700

    Add roctracer plugin functions

--- lib/hipfort/hipfort_roctracer.f
+++ lib/hipfort/hipfort_roctracer.f
@@ -127,4 +127,215 @@ module hipfort_roctracer
     end function
   end interface
 
+  interface roctracer_next_record
+    function roctracer_next_record_() bind(c, name="roctracer_next_record")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_next_record_
+      integer(kind(ACTIVITY_DOMAIN_HSA_API)) :: domain
+      type(c_ptr),value :: record
+      type(c_ptr),value :: next
+    end function
+  end interface
+
+  interface roctracer_open_pool_expl
+    function roctracer_open_pool_expl_() bind(c, name="roctracer_open_pool_expl")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_open_pool_expl_
+      type(c_ptr),value :: properties
+      type(c_ptr),value :: pool
+    end function
+  end interface
+
+  interface roctracer_close_pool_expl
+    function roctracer_close_pool_expl_() bind(c, name="roctracer_close_pool_expl")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_close_pool_expl_
+      type(c_ptr),value :: properties
+    end function
+  end interface
+
+  interface roctracer_close_pool
+    function roctracer_close_pool_() bind(c, name="roctracer_close_pool")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_close_pool_
+    end function
+  end interface
+
+  interface roctracer_default_pool_expl
+    function roctracer_default_pool_expl_() bind(c, name="roctracer_default_pool_expl")
+      use iso_c_binding
+      implicit none
+      type(c_ptr),value :: roctracer_default_pool_expl_
+      type(c_ptr),value :: pool
+    end function
+  end interface
+
+  interface roctracer_default_pool
+    function roctracer_default_pool_() bind(c, name="roctracer_default_pool")
+      use iso_c_binding
+      implicit none
+      type(c_ptr),value :: roctracer_default_pool_
+    end function
+  end interface
+
+  interface roctracer_enable_op_activity_expl
+    function roctracer_enable_op_activity_expl_() bind(c, name="roctracer_enable_op_activity_expl")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_enable_op_activity_expl_
+      integer(kind(ACTIVITY_DOMAIN_HSA_API)) :: domain
+      integer(C_INT32_T),value :: op
+      type(c_ptr),value :: pool
+    end function
+  end interface
+
+  interface roctracer_enable_op_activity
+    function roctracer_enable_op_activity_() bind(c, name="roctracer_enable_op_activity")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_enable_op_activity_
+      integer(kind(ACTIVITY_DOMAIN_HSA_API)) :: domain
+      integer(C_INT32_T),value :: op
+    end function
+  end interface
+
+  interface roctracer_enable_domain_activity_expl
+    function roctracer_enable_domain_activity_expl_() bind(c, name="roctracer_enable_domain_activity_expl")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_enable_domain_activity_expl_
+      integer(kind(ACTIVITY_DOMAIN_HSA_API)) :: domain
+      type(c_ptr),value :: pool
+    end function
+  end interface
+
+  interface roctracer_enable_domain_activity
+    function roctracer_enable_domain_activity_() bind(c, name="roctracer_enable_domain_activity")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_enable_domain_activity_
+      integer(kind(ACTIVITY_DOMAIN_HSA_API)) :: domain
+    end function
+  end interface
+
+  interface roctracer_disable_op_activity
+    function roctracer_disable_op_activity_() bind(c, name="roctracer_disable_op_activity")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_disable_op_activity_
+      integer(kind(ACTIVITY_DOMAIN_HSA_API)) :: domain
+      integer(C_INT32_T),value :: op
+    end function
+  end interface
+
+  interface roctracer_disable_domain_activity
+    function roctracer_disable_domain_activity_() bind(c, name="roctracer_disable_domain_activity")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_disable_domain_activity_
+      integer(kind(ACTIVITY_DOMAIN_HSA_API)) :: domain
+      integer(C_INT32_T),value :: op
+    end function
+  end interface
+
+  interface roctracer_flush_activity_expl
+    function roctracer_flush_activity_expl_() bind(c, name="roctracer_flush_activity_expl")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_flush_activity_expl_
+      type(c_ptr),value :: pool
+    end function
+  end interface
+
+  interface roctracer_flush_activity
+    function roctracer_flush_activity_() bind(c, name="roctracer_flush_activity")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_flush_activity_
+    end function
+  end interface
+
+  interface roctracer_get_timestamp
+    function roctracer_get_timestamp_() bind(c, name="roctracer_get_timestamp")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_get_timestamp_
+      integer(C_INT64_T) :: timestamp
+    end function
+  end interface
+
+  interface roctracer_start
+    subroutine roctracer_start_() bind(c, name="roctracer_start")
+      use iso_c_binding
+      implicit none
+    end subroutine
+  end interface
+
+  interface roctracer_stop
+    subroutine roctracer_stop_() bind(c, name="roctracer_stop")
+      use iso_c_binding
+      implicit none
+    end subroutine
+  end interface
+
+  interface roctracer_activity_push_external_correlation_id
+    function roctracer_activity_push_external_correlation_id_() bind(c, name="roctracer_activity_push_external_correlation_id")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_activity_push_external_correlation_id_
+      integer(C_INT64_T),value :: id
+    end function
+  end interface
+
+  interface roctracer_activity_pop_external_correlation_id
+    function roctracer_activity_pop_external_correlation_id_() bind(c, name="roctracer_activity_pop_external_correlation_id")
+      use iso_c_binding
+      implicit none
+      integer(kind(ROCTRACER_STATUS_SUCCESS)) :: roctracer_activity_pop_external_correlation_id_
+      type(c_ptr),value :: last_id
+    end function
+  end interface
+
+  interface roctracer_plugin_initialize
+    function roctracer_plugin_initialize_() bind(c, name="roctracer_plugin_initialize")
+      use iso_c_binding
+      implicit none
+      integer(C_INT) :: roctracer_plugin_initialize_
+      integer(C_INT32_T) :: roctracer_major_version
+      integer(C_INT32_T) :: roctracer_minor_version
+    end function
+  end interface
+
+  interface roctracer_plugin_finalize
+    subroutine roctracer_plugin_finalize_() bind(c, name="roctracer_plugin_finalize")
+      use iso_c_binding
+      implicit none
+      integer(C_INT32_T) :: roctracer_plugin_finalize_
+    end subroutine
+  end interface
+
+  interface roctracer_plugin_write_callback_record
+    function roctracer_plugin_write_callback_record_() bind(c, name="roctracer_plugin_write_callback_record")
+      use iso_c_binding
+      implicit none
+      integer(C_INT) :: roctracer_plugin_write_callback_record_
+      integer(c_ptr) :: record
+      integer(c_ptr) :: callback_data
+    end function
+  end interface
+
+  interface roctracer_plugin_write_activity_records
+    function roctracer_plugin_write_activity_records_() bind(c, name="roctracer_plugin_write_activity_records")
+      use iso_c_binding
+      implicit none
+      integer(C_INT) :: roctracer_plugin_write_activity_records_
+      integer(c_ptr) :: begin
+      integer(c_ptr) :: end
+    end function
+  end interface
+
 end module hipfort_roctracer
